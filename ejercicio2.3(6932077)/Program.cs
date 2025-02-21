68a06c673541075aea2ff211f9e8dc82534357ae
NicoleJesy <nikkynico.arevalo@gmail.com>
2025-02-20 21:35:44 -0600

    Add files via upload

---
+++ ejercicio2.3(6932077)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2._3_6932077_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        decimal MajonchoCost;
+        int CantidadadMajonchos;
+        decimal TotalMajonchocost;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MajonchoCost = Convert.ToDecimal(costo.Text);
+            CantidadadMajonchos = Convert.ToInt32(cantidad.Text);
+            TotalMajonchocost = MajonchoCost * CantidadadMajonchos;
+            total.Text = "Pagar: " + Convert.ToString(TotalMajonchocost);
+        }
+
+    }
+}
+
+
