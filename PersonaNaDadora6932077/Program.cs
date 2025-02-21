fbfeb389c1ccd9b0de095c63a2629f1d02f7fd0b
NicoleJesy <nikkynico.arevalo@gmail.com>
2025-02-20 21:37:23 -0600

    Add files via upload

---
+++ PersonaNaDadora6932077/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaNaDadora6932077
+{
+    public class PersonaNaDadora:Persona
+    {
+        public PersonaNaDadora(string ojos, string altua, string peso, string sexo) : base(ojos, altura, peso, sexo,)  {
+        }
+    }
+    public void Nadar()
+    {
+        //la practica de la natación hará volver al peso inical 
+        Console.WriteLine("Nadando para volver al peso ideal");
+        this.peso = "85";
+    }
+
+}
